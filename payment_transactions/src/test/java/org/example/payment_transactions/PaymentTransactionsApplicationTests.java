37e138edd1b133538010cfa928a9c7ca4b776806
samuelmarcos <samuelmarcos9580@gmail.com>
2024-03-09 12:52:31 -0300

    setup spring

---
+++ payment_transactions/src/test/java/org/example/payment_transactions/PaymentTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.payment_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
